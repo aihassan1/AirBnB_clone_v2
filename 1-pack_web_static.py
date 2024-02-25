94e6cd2f7e513ad3fa4833697402ac093b2c8ced
aihassan1 <abdosruor57@gmail.com>
2024-02-25 07:26:55 +0200

    1-pack_web_static.py

---
+++ 1-pack_web_static.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+from fabric.api import task, local
+
+
+@task
+def do_pack():
+    """Generates a .tgz archive from the contents of the web_static"""
+
+    result = local("date +%Y%m%d%H%M%S", capture=True)
+    formatted_time = result.strip()
+    local("mkdir -p versions")
+    output = local(
+        f"tar -czvf versions/web_static_{formatted_time}.tgz versions/web_static"
+    )
+    if output.succeeded:
+        return f"versions/web_static_{formatted_time}.tgz"
+    else:
+        return None
---
+++ fabfile.py
@@ -0,0 +1,27 @@
+from fabric import task
+
+from datetime import datetime
+from fabric.api import local
+
+
+def do_pack():
+    """generates a tgz archive"""
+    try:
+        date = datetime.now().strftime("%Y%m%d%H%M%S")
+        if isdir("versions") is False:
+            local("mkdir versions")
+        file_name = "versions/web_static_{}.tgz".format(date)
+        local("tar -cvzf {} web_static".format(file_name))
+        return file_name
+    except:
+        return None
+
+
+
+
+
+
+@task
+def hello(c):
+    print("Hello from Fabric!")
+
