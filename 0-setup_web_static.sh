40b2cf39a273bce99f5fc6a8bf381a0ef6463976
aihassan1 <abdosruor57@gmail.com>
2024-02-25 05:12:24 +0200

    0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -2,16 +2,9 @@
 # configer the server to be ready for deployment
 sudo apt-get update
 sudo apt-get -y install nginx
-sudo touch /data/web_static/releases/test/index.html
 
-# Create a basic HTML file
-echo "<html>
-    <head>
-    </head>
-    <body>
-        Holberton School
-    </body>
-    </html>" >/data/web_static/releases/test/index.html
+mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/shared/
 
 # create a new symbolic link
 sudo ln -sf /data/web_static/releases/test /data/web_static/current
