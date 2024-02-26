81c7140eb957b4a251c3a7cb0f81dfbe9eef02ae
aihassan1 <abdosruor57@gmail.com>
2024-02-27 00:26:35 +0200

    0-hello_route.py

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -9,8 +9,8 @@ app = Flask(__name__)
 @app.route("/", strict_slashes=False)
 def hello():
     """Return a greeting message."""
-    return "Hello HBNB"
+    return "Hello HBNB!"
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000)
