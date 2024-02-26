90c2f2163cb0f16bca258581b1f7be2947d0926b
aihassan1 <abdosruor57@gmail.com>
2024-02-27 01:07:40 +0200

    3-python_route.py

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -26,8 +26,8 @@ def c_is_fun(text):
     return f"C {text}"
 
 
-@app.route("/python/<text>")
-def python_is_cool(text="is cool", strict_slashes=False):
+@app.route("/python/<text>", strict_slashes=False)
+def python_is_cool(text="is cool"):
     """returns python + text"""
     text = text.replace("_", " ")
     return f"Python {text}"
